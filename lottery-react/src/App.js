1a67fba9a62c5fa209950202e329af391a1bbe4e
paigexx <pcjones12@gmail.com>
2022-01-15 15:08:37 -0600

    added enter fun rendered contract info on app.js

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,28 +1,52 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
 import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends React.Component {
-  render() {
-    web3.eth.getAccounts().then(console.log);
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: ""
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
+  }
 
+  onSubmit = async (event) => {
+    event.preventDefault()
+    const accounts = await web3.eth.getAccounts()
+
+    this.setState({message: "Waiting on transaction success.. "})
+
+    await lottery.methods.enter().send({from: accounts[0], value: web3.utils.toWei(this.state.value, "ether" )})
+
+    this.setState({message: "You have been entered in the lottery."})
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently {" "} 
+          {this.state.players.length} people entered, competing to win: {" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount to enter: </label>
+            <input value = {this.state.value} onChange={event => this.setState({value: event.target.value})}></input>
+          </div>
+          <button>Enter</button>    
+        </form>
+        <hr/>
+        <h4 color="red">{this.state.message}</h4>
       </div>
     );
   }
---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,12 @@
+import web3 from "./web3"
+
+// receive from console log of deploy.js
+
+const abi = [
+    {"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}
+]
+
+const address = "0x2701BFdB65B980088299Ed191e73A1fFdF3A8c92"
+
+// creates local instance of contract to be used in the react application 
+export default new web3.eth.Contract(abi, address)
